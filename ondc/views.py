2f66fd8ec0b69edc31177138d69be339e4e8fd48
amalnath369 <amalnaths640@gmail.com>
2025-04-26 16:02:27 +0530

    Initial commit

--- ondc/views.py
+++ ondc/views.py
@@ -6,6 +6,7 @@ from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from .cryptic_utils import decrypt
 
+
 # Load from environment variables
 SIGNED_UNIQUE_REQ_ID = os.environ.get("SIGNED_UNIQUE_REQ_ID")
 ENCRYPTION_PRIVATE_KEY_BASE64 = os.environ.get("ENCRYPTION_PRIVATE_KEY")
