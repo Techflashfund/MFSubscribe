b1b2dccc0140ac34eb17ae91f450d7243e879315
amalnath369 <amalnaths640@gmail.com>
2025-04-26 17:46:04 +0530

    update bug

--- ondc/urls.py
+++ ondc/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ondc_site_verification, on_subscribe
 
 urlpatterns = [
-    path('callback/on_subscribe', on_subscribe, name='on_subscribe')    
+    path('on_subscribe', on_subscribe, name='on_subscribe')    
 ]
